3b511f625d434f922e0033a4bed646acd24adbd8
ykang1001 <y.kang@rochester.edu>
2021-11-11 14:37:59 -0500

    cleaned scc on first page

--- augur/src/App.js
+++ augur/src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
-import augurlogo from './images/augurlogo.png';
-import arrow from './images/arrowpg1.png'
-import './app-copy.css';
+// import logo from "./logo.svg";
+import augurlogo from "./images/augurlogo.png";
+import arrow from "./images/arrowpg1.png";
+import "./app-copy.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src={augurlogo} className="augurlogo" alt="logo"/>
-        <h1>Hello</h1>
-        <h2>we are <emph>augur</emph></h2>
-        <a className="arrow-nav"
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer">and we are here to show your future</a>
-        <img src={arrow} alt="arrow"></img>
+
+        <a href="/" target="_blank" rel="noopener noreferrer">
+          {" "}
+          <img src={augurlogo} className="augurlogo" alt="logo" />{" "}
+        </a>
+        <div className="title-hero">
+          <h1>Hello</h1>
+          <h2>
+            we are <bold>augur</bold>
+          </h2>
+        </div>
+        <a className="arrow-nav" href="/" target="_blank" rel="noopener noreferrer">
+          and we are here to show your future  
+          <img src={arrow} className="arrow"alt="arrow"></img>
+        </a>
+
         {/* <a
           className="App-link"
           href="https://reactjs.org"
@@ -26,7 +34,6 @@ function App() {
         </a> */}
       </header>
     </div>
-
 
     // <div className="App">
     //   <header className="App-header">
