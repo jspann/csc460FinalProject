3014badfb9cb15f7e410ea81fb4255fadc8c4a87
James S <jspann2@ur.rochester.edu>
2021-12-02 13:58:10 -0500

    updated drag and drop

--- augur/api/api.py
+++ augur/api/api.py
@@ -1,5 +1,7 @@
 import time
 from flask import Flask
+from flask import request
+
 
 app = Flask(__name__)
 @app.route('/hello', methods=['GET', 'POST'])
@@ -10,5 +12,23 @@ def hello_world():
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/submitPhoto', methods=['GET', 'POST'])
+def submitPhoto():
+    try:
+        print(request.form.get('myjsonkey'))
+        # print(request.args.get('file'))
+        # print(request.form.get('file'))
+        # print("form:", request.form.keys())
+        # print("args:", request.args.keys())
+        # print("files:", request.files.keys())
+        for fle in request.files.keys():
+            print(request.files.get(fle).save("content_length.JPG"))
+
+        return "words"
+    except Exception as e:
+        # raise e
+        print("We got the exception: ",e)
+        return "Nah"
 
 app.run('127.0.0.1', port = 5000)
--- augur/src/components/CrystalBall.jsx
+++ augur/src/components/CrystalBall.jsx
@@ -1,5 +1,19 @@
 import { FileDrop } from "react-file-drop";
 import "../app-copy.css";
+/*
+function setPhotoFormData(){
+  var data = new FormData();
+  const payload = {
+      // id: self.refs.id,
+      // studentName: self.refs.sname,
+      // age: self.refs.age,
+      // emailId: self.refs.emailId
+      // original_image: 
+
+  };
+  data.append("myjsonkey", JSON.stringify(payload));
+  return data;
+}*/
 
 function CrystalBall() {
   // const fileInputRef = useRef(null);
@@ -16,10 +30,42 @@ function CrystalBall() {
         <FileDrop
           onFrameDragEnter={(event) => console.log("onFrameDragEnter", event)}
           onFrameDragLeave={(event) => console.log("onFrameDragLeave", event)}
-          onFrameDrop={(event) => console.log("onFrameDrop", event)}
-          onDragOver={(event) => console.log("onDragOver", event)}
-          onDragLeave={(event) => console.log("onDragLeave", event)}
-          onDrop={(files, event) => console.log("onDrop!", files, event)}
+          onFrameDrop={(event) => {}}
+          onDrop={(files, event) => {
+            console.log('onDrop!', files, event);
+            console.log(typeof(files[0]));
+            event.preventDefault();
+
+            var data = new FormData();
+            const payload = {
+            // id: self.refs.id,
+            // studentName: self.refs.sname,
+            // age: self.refs.age,
+            // emailId: self.refs.emailId
+              whoami: "crystalball"
+
+            };
+            data.append("myjsonkey", JSON.stringify(payload));
+            // data.append("file", files[0]);
+            for (let i = 0; i < files.length; i++) {
+              console.log("looping!");
+            data.append(`images[${i}]`, files[i]);
+            }
+
+            // console.log("heart:",data);
+
+              // Send POST
+              fetch('/submitPhoto', {
+                method: 'POST',
+                body: data
+              }).then(function(response) {
+                if(response.ok) {
+                 return alert("And the server replied:",response);
+               }
+             });
+            }
+          }
+
         >
           <div className="__act crys">
             drop a photo of your home into the crystal and we’ll show you
